42803a0e15277b80b762a17cfdc00435ddc19369
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-11 16:30:26 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/lucas/virtualBackAPI/VirtualBackApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.virtualBackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/virtualBackAPI/VirtualBackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.virtualBackAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
