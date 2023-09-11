5a06917929e52736bedb2d51b41f9910735e5eda
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-11 16:36:01 -0300

    Criando classe de dominio Usuario

---
+++ src/main/java/br/com/lucas/virtualBackAPI/domain/Usuario.java
@@ -0,0 +1,22 @@
+package br.com.lucas.virtualBackAPI.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Data
+public class Usuario {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
