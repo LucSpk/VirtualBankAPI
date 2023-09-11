e33a9aafca3d922a8569a9a0103bb8e2bba430d4
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-11 17:18:17 -0300

    Modifica o User Controller para acessar a camada de services

--- src/main/java/br/com/lucas/virtualBankAPI/controllers/users/UserController.java
+++ src/main/java/br/com/lucas/virtualBankAPI/controllers/users/UserController.java
@@ -1,6 +1,8 @@
 package br.com.lucas.virtualBankAPI.controllers.users;
 
 import br.com.lucas.virtualBankAPI.domain.Usuario;
+import br.com.lucas.virtualBankAPI.services.userServices.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController @RequestMapping("ap1/v1/user")
 public class UserController {
 
+    @Autowired
+    private UserServices userServices;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new Usuario(1, "Lucas Alves", "lucas@email.com", "123456"));
+        return ResponseEntity.ok().body(userServices.findById(id));
     }
 }
