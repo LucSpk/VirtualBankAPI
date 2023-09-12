588e5b8a2f45f45c2be54deadefe9dec79f0c05a
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-12 18:38:55 -0300

    Cria endpoint para criar um novo usuario

--- src/main/java/br/com/lucas/virtualBankAPI/controllers/users/UserController.java
+++ src/main/java/br/com/lucas/virtualBankAPI/controllers/users/UserController.java
@@ -1,16 +1,16 @@
 package br.com.lucas.virtualBankAPI.controllers.users;
 
+import br.com.lucas.virtualBankAPI.domain.users.CreateUserRequest;
 import br.com.lucas.virtualBankAPI.domain.users.Usuario;
-import br.com.lucas.virtualBankAPI.domain.users.dto.UsuarioDTO;
+import br.com.lucas.virtualBankAPI.domain.users.UsuarioDTO;
 import br.com.lucas.virtualBankAPI.services.userServices.UserServices;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +36,13 @@ public class UserController {
                                 (user) -> modelMapper.map(user, UsuarioDTO.class)
                         ).collect(Collectors.toList())
         );
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> create(@RequestBody CreateUserRequest request) {
+        UsuarioDTO userDTO = modelMapper.map(request, UsuarioDTO.class);
+        UsuarioDTO userCreateDTO = userServices.create(userDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userCreateDTO.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
