dc501782daa9fa84b43fc336fa86620970251417
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-14 16:09:31 -0300

    Cria classe de dominio account

---
+++ src/main/java/br/com/lucas/virtualBankAPI/domain/accounts/Account.java
@@ -0,0 +1,24 @@
+package br.com.lucas.virtualBankAPI.domain.accounts;
+
+import br.com.lucas.virtualBankAPI.domain.users.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String accNumber;
+    private Double balance;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Usuario owner;
+}
