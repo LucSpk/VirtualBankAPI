4757e19f025cc5ab447f41908cf6392e84c3554f
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-15 16:44:46 -0300

    Implementa findById para o AccountServices

--- src/main/java/br/com/lucas/virtualBankAPI/services/accounts/impl/AccountServicesImpl.java
+++ src/main/java/br/com/lucas/virtualBankAPI/services/accounts/impl/AccountServicesImpl.java
@@ -1,6 +1,44 @@
 package br.com.lucas.virtualBankAPI.services.accounts.impl;
 
+import br.com.lucas.virtualBankAPI.domain.accounts.Account;
+import br.com.lucas.virtualBankAPI.domain.accounts.AccountDTO;
+import br.com.lucas.virtualBankAPI.enums.exceptions.ErrorMessage;
+import br.com.lucas.virtualBankAPI.repositories.accounts.AccountRepository;
 import br.com.lucas.virtualBankAPI.services.accounts.AccountServices;
+import br.com.lucas.virtualBankAPI.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
 
 public class AccountServicesImpl implements AccountServices {
+
+    private AccountRepository accountRepository;
+    private ModelMapper modelMapper;
+
+    @Override
+    public AccountDTO findById(Long id) {
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(ErrorMessage.OBJETO_NAO_ENCONTRADO.getMessage()));
+        return modelMapper.map(account, AccountDTO.class);
+    }
+
+    @Override
+    public List<AccountDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public AccountDTO create(Account usuario) {
+        return null;
+    }
+
+    @Override
+    public AccountDTO update(Account usuario, Integer id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
 }
