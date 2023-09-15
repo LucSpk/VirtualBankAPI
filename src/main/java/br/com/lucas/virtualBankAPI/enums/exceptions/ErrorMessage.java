6c854c95235d4bc9beda870970fadebd23da02ce
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-15 18:03:41 -0300

    Cria mensagem de erro no ENUM para numero de conta ja cadastrado.

--- src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
+++ src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
@@ -3,6 +3,7 @@ package br.com.lucas.virtualBankAPI.enums.exceptions;
 public enum ErrorMessage {
     OBJETO_NAO_ENCONTRADO("Objeto não encontrado"),
     EAMIL_JA_CADASTRADO("E-mail já cadastrado no sistema"),
+    NUMERO_ACC_JA_CADASTRADO("Número da conta já existe no sistem"),
     DIVERGENCIA_NOS_DADOS("Há divergencia nos dados");
 
     private final String message;
