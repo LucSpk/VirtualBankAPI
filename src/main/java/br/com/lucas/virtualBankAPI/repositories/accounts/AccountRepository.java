936ccd6901eacbc9e311b24efbf4d6b9573f8e88
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-18 15:50:21 -0300

    Implementa findByAccNumber para a AccountRepository

--- src/main/java/br/com/lucas/virtualBankAPI/repositories/accounts/AccountRepository.java
+++ src/main/java/br/com/lucas/virtualBankAPI/repositories/accounts/AccountRepository.java
@@ -3,5 +3,9 @@ package br.com.lucas.virtualBankAPI.repositories.accounts;
 import br.com.lucas.virtualBankAPI.domain.accounts.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccNumber(String accNumber);
 }
