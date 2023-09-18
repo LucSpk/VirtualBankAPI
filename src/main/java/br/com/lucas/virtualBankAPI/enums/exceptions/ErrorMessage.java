b86ee1546ee3f18a22b9bbdc12e3c10338765f02
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-18 17:52:23 -0300

    Criar novas mensagens

--- src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
+++ src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
     OBJETO_NAO_ENCONTRADO("Objeto não encontrado"),
     EAMIL_JA_CADASTRADO("E-mail já cadastrado no sistema"),
     NUMERO_ACC_JA_CADASTRADO("Número da conta já existe no sistem"),
-    DIVERGENCIA_NOS_DADOS("Há divergencia nos dados");
+    DIVERGENCIA_NOS_DADOS("Há divergencia nos dados"),
+    USUARIO_NAO_ENCONTRADO("Usuário não encontrado"),
+    NUM_ACC_NAO_ENCONTRADO("Número de Conta não encontrado");
 
     private final String message;
 
