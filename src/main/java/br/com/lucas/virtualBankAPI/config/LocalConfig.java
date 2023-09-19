9b12cacf316ee1aa0c94abaeb2adc82cc4a9507d
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-19 16:42:15 -0300

    refactor(LocalConfig): Não inicia banco local
    Comenta linhas que iniciam banco local

--- src/main/java/br/com/lucas/virtualBankAPI/config/LocalConfig.java
+++ src/main/java/br/com/lucas/virtualBankAPI/config/LocalConfig.java
@@ -21,15 +21,15 @@ public class LocalConfig {
 
     @Bean
     public void startDB() {
-        Usuario user01 = new Usuario(null, "Lucas", "lucas@email.com", "123456");
-        Usuario user02 = new Usuario(null, "Fulano", "fulano@email.com", "123456");
-
-        userRepository.saveAll(List.of(user01, user02));
-
-        Account account01 = new Account(null, "123456789", 100.0, user01);
-        Account account02 = new Account(null, "535353535", 300.0, user02);
-
-        accountRepository.saveAll(List.of(account01, account02));
+//        Usuario user01 = new Usuario(null, "Lucas", "lucas@email.com", "123456");
+//        Usuario user02 = new Usuario(null, "Fulano", "fulano@email.com", "123456");
+//
+//        userRepository.saveAll(List.of(user01, user02));
+//
+//        Account account01 = new Account(null, "123456789", 100.0, user01);
+//        Account account02 = new Account(null, "535353535", 300.0, user02);
+//
+//        accountRepository.saveAll(List.of(account01, account02));
 
     }
 
