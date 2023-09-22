99c29006e685c28bb93e0f0dc25b6ff3bd09fb5b
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-22 11:49:30 -0300

    feat(TransactionsServices): Inicia classe de service para Transactions
    Inicia classe de service para Transactions

--- src/main/java/br/com/lucas/virtualBankAPI/services/transactions/TransactionServices.java
+++ src/main/java/br/com/lucas/virtualBankAPI/services/transactions/TransactionServices.java
@@ -1,4 +1,15 @@
 package br.com.lucas.virtualBankAPI.services.transactions;
 
+import br.com.lucas.virtualBankAPI.domain.transactions.Transaction;
+import br.com.lucas.virtualBankAPI.domain.transactions.TransactionDTO;
+
+import java.util.List;
+
 public interface TransactionServices {
+
+    TransactionDTO findById(Long id);
+    List<TransactionDTO> findAll();
+    TransactionDTO create(Transaction transaction, Long sourceAccountId, Long destinationAccount);
+    TransactionDTO update(Transaction transaction, Long id);
+    void delete(Long id);
 }
--- src/main/java/br/com/lucas/virtualBankAPI/services/transactions/impl/TransactionServicesImpl.java
+++ src/main/java/br/com/lucas/virtualBankAPI/services/transactions/impl/TransactionServicesImpl.java
@@ -1,6 +1,45 @@
 package br.com.lucas.virtualBankAPI.services.transactions.impl;
 
+import br.com.lucas.virtualBankAPI.domain.transactions.Transaction;
+import br.com.lucas.virtualBankAPI.domain.transactions.TransactionDTO;
+import br.com.lucas.virtualBankAPI.repositories.transactions.TransactionRepository;
 import br.com.lucas.virtualBankAPI.services.transactions.TransactionServices;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class TransactionServicesImpl implements TransactionServices {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public TransactionDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TransactionDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public TransactionDTO create(Transaction transaction, Long sourceAccountId, Long destinationAccount) {
+        return null;
+    }
+
+    @Override
+    public TransactionDTO update(Transaction transaction, Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
