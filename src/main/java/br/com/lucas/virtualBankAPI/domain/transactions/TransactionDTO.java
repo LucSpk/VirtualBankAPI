fba6977c3030d59576e0ade063681a65e7de4d2b
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-22 11:48:39 -0300

    fix(TransactionDTO): Muda atributos
    Troca atributos da classe de TransactionsDTO de Account para AccountDTO

--- src/main/java/br/com/lucas/virtualBankAPI/domain/transactions/TransactionDTO.java
+++ src/main/java/br/com/lucas/virtualBankAPI/domain/transactions/TransactionDTO.java
@@ -1,23 +1,20 @@
 package br.com.lucas.virtualBankAPI.domain.transactions;
 
-import br.com.lucas.virtualBankAPI.domain.accounts.Account;
-import jakarta.persistence.*;
+import br.com.lucas.virtualBankAPI.domain.accounts.AccountDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 
-@NoArgsConstructor @AllArgsConstructor
-@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Getter
 public class TransactionDTO {
 
-    private Long transactionId;
+    private Long id;
     private String transactionType;
     private Double amount;
     private LocalDateTime timestamp;
-    private Account sourceAccount;
-    private Account destinationAccount;
+    private AccountDTO sourceAccount;
+    private AccountDTO destinationAccount;
 }
