9b234e454c127a5b8a47e828074ac9cdd9aa398b
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-22 16:25:26 -0300

    feat(TransactionsServiceImp): implementa metodos read
    Cria metodos de findAll e findById

--- src/main/java/br/com/lucas/virtualBankAPI/services/transactions/impl/TransactionServicesImpl.java
+++ src/main/java/br/com/lucas/virtualBankAPI/services/transactions/impl/TransactionServicesImpl.java
@@ -2,13 +2,16 @@ package br.com.lucas.virtualBankAPI.services.transactions.impl;
 
 import br.com.lucas.virtualBankAPI.domain.transactions.Transaction;
 import br.com.lucas.virtualBankAPI.domain.transactions.TransactionDTO;
+import br.com.lucas.virtualBankAPI.enums.exceptions.ErrorMessage;
 import br.com.lucas.virtualBankAPI.repositories.transactions.TransactionRepository;
+import br.com.lucas.virtualBankAPI.services.exceptions.ObjectNotFoundException;
 import br.com.lucas.virtualBankAPI.services.transactions.TransactionServices;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionServicesImpl implements TransactionServices {
@@ -20,12 +23,15 @@ public class TransactionServicesImpl implements TransactionServices {
 
     @Override
     public TransactionDTO findById(Long id) {
-        return null;
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(ErrorMessage.TRANSACTION_NAO_ENCONTRADO.getMessage()));
+        return modelMapper.map(transaction, TransactionDTO.class);
     }
 
     @Override
     public List<TransactionDTO> findAll() {
-        return null;
+        List<Transaction> response = transactionRepository.findAll();
+        return response.stream().map((trs) -> modelMapper.map(trs, TransactionDTO.class)).collect(Collectors.toList());
     }
 
     @Override
