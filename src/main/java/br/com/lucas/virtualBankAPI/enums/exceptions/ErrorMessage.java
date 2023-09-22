1f00b29e0af53af246ad2d9e99c6ba9efc317512
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-22 18:28:42 -0300

    feat(ErrorMessage): Cria mensagens de error
    Cria mensagens de error

--- src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
+++ src/main/java/br/com/lucas/virtualBankAPI/enums/exceptions/ErrorMessage.java
@@ -7,7 +7,12 @@ public enum ErrorMessage {
     DIVERGENCIA_NOS_DADOS("Há divergencia nos dados"),
     USUARIO_NAO_ENCONTRADO("Usuário não encontrado"),
     NUM_ACC_NAO_ENCONTRADO("Número de Conta não encontrado"),
-    TRANSACTION_NAO_ENCONTRADO("Transação não encontrada");
+    TRANSACTION_NAO_ENCONTRADO("Transação não encontrada"),
+    CONTAS_DEVEM_SER_FORNECIDAS("As contas de origem e destino devem ser fornecidas"),
+    VALOR_MENOR_QUE_ZERO("O valor da transferência deve ser maior que zero"),
+    SALDO_INSUFICIENTE("Saldo insuficiente na conta de origem"),
+    TRANSACAO_NAO_ENCONTRADA("A transação que esta tentando fazer não existe")
+    ;
 
     private final String message;
 
