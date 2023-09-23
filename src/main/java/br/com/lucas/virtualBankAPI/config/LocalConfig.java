ca9a32f2f850ab84ab935d0a0cb557859cbee1de
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-23 19:39:29 -0300

    refactor(TransactionServiceImp): organiza codigo
    organiza codigo

--- src/main/java/br/com/lucas/virtualBankAPI/config/LocalConfig.java
+++ src/main/java/br/com/lucas/virtualBankAPI/config/LocalConfig.java
@@ -1,14 +1,20 @@
 package br.com.lucas.virtualBankAPI.config;
 
 import br.com.lucas.virtualBankAPI.domain.accounts.Account;
+import br.com.lucas.virtualBankAPI.domain.transactions.Transaction;
+import br.com.lucas.virtualBankAPI.domain.transactions.TransactionRequest;
 import br.com.lucas.virtualBankAPI.domain.users.Usuario;
+import br.com.lucas.virtualBankAPI.enums.transactions.TransactionType;
 import br.com.lucas.virtualBankAPI.repositories.accounts.AccountRepository;
+import br.com.lucas.virtualBankAPI.repositories.transactions.TransactionRepository;
 import br.com.lucas.virtualBankAPI.repositories.users.UserRepository;
+import br.com.lucas.virtualBankAPI.services.transactions.TransactionServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Configuration @Profile("local")
@@ -18,6 +24,8 @@ public class LocalConfig {
     private UserRepository userRepository;
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private TransactionServices transactionServices;
 
     @Bean
     public void startDB() {
@@ -30,6 +38,17 @@ public class LocalConfig {
 //        Account account02 = new Account(null, "535353535", 300.0, user02);
 //
 //        accountRepository.saveAll(List.of(account01, account02));
+//
+//        TransactionRequest request01 = new TransactionRequest(TransactionType.DEPOSIT, null, account01.getId(), 200.0);
+//        this.transactionServices.transaction(request01);
+//        TransactionRequest request02 = new TransactionRequest(TransactionType.DEPOSIT, null, account02.getId(), 375.0);
+//        this.transactionServices.transaction(request02);
+//        TransactionRequest request03 = new TransactionRequest(TransactionType.TRANSFER, account02.getId(), account01.getId(), 25.0);
+//        this.transactionServices.transaction(request03);
+//        TransactionRequest request04 = new TransactionRequest(TransactionType.PAYMENT, account01.getId(), account02.getId(), 50.0);
+//        this.transactionServices.transaction(request04);
+//        TransactionRequest request05 = new TransactionRequest(TransactionType.WITHDRAW, account01.getId(), null, 15.0);
+//        this.transactionServices.transaction(request05);
 
     }
 
