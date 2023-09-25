e3bcbea8edd479e57fbdb4c1b82fb661090b31ed
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-09-25 16:40:18 -0300

    refactor(Account): Adiciona atributos
    Adiciona atributos de transacao para account e AccountDTO

--- src/main/java/br/com/lucas/virtualBankAPI/domain/accounts/Account.java
+++ src/main/java/br/com/lucas/virtualBankAPI/domain/accounts/Account.java
@@ -1,11 +1,15 @@
 package br.com.lucas.virtualBankAPI.domain.accounts;
 
+import br.com.lucas.virtualBankAPI.domain.transactions.Transaction;
 import br.com.lucas.virtualBankAPI.domain.users.Usuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor @AllArgsConstructor
@@ -22,9 +26,21 @@ public class Account {
     @JoinColumn(name = "user_id")
     private Usuario owner;
 
+    @OneToMany(mappedBy = "sourceAccount", fetch = FetchType.EAGER)
+    private List<Transaction> outgoingTransactions = new ArrayList<>();
+    @OneToMany(mappedBy = "destinationAccount", fetch = FetchType.EAGER)
+    private List<Transaction> incomingTransactions = new ArrayList<>();
+
     public Account(Long id, String accNumber, Double balance) {
         this.id = id;
         this.accNumber = accNumber;
         this.balance = balance;
+    }
+
+    public Account(Long id, String accNumber, Double balance, Usuario owner) {
+        this.id = id;
+        this.accNumber = accNumber;
+        this.balance = balance;
+        this.owner = owner;
     }
 }
--- src/main/java/br/com/lucas/virtualBankAPI/domain/accounts/AccountDTO.java
+++ src/main/java/br/com/lucas/virtualBankAPI/domain/accounts/AccountDTO.java
@@ -1,7 +1,12 @@
 package br.com.lucas.virtualBankAPI.domain.accounts;
 
+import br.com.lucas.virtualBankAPI.domain.transactions.TransactionDTO;
 import br.com.lucas.virtualBankAPI.domain.users.UsuarioDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class AccountDTO {
@@ -10,10 +15,21 @@ public class AccountDTO {
     private String accNumber;
     private Double balance;
     private UsuarioDTO owner;
+    @JsonIgnore
+    private List<TransactionDTO> outgoingTransactions = new ArrayList<>();
+    @JsonIgnore
+    private List<TransactionDTO> incomingTransactions = new ArrayList<>();
 
     public AccountDTO(Long id, String accNumber, Double balance) {
         this.id = id;
         this.accNumber = accNumber;
         this.balance = balance;
+    }
+
+    public AccountDTO(Long id, String accNumber, Double balance, UsuarioDTO owner) {
+        this.id = id;
+        this.accNumber = accNumber;
+        this.balance = balance;
+        this.owner = owner;
     }
 }
